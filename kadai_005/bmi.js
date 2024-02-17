dc055dfd1e3682834eb2296558151e9aea6e40c9
suu826 <157122010+suu826@users.noreply.github.com>
2024-02-17 20:13:23 +0900

    kadai_005

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,3 +1,15 @@
+let userNumber1;
+
+let userNamer2;
+
+let userNamer3;
+
+userName1 = "68";
+
+userName2 = "1.7";
+
+userName3 = "1.7";
+
 let userNumber = 68 / (1.7 * 1.7);
 
 console.log(userNumber);
