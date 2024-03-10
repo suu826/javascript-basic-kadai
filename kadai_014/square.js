758f8ec6fb24baf254bb1048c2d73c23e65c08ed
suu826 <157122010+suu826@users.noreply.github.com>
2024-03-10 17:38:34 +0900

    kadai_014

---
+++ kadai_014/square.js
@@ -0,0 +1,12 @@
+// 引数を渡す
+const calculateTotal = (price) => {
+    console.log(price + 50);
+};
+
+// 与えられた引数numを二乗にし、その値を戻り値として返す関数を定義する
+const double = (num) => {
+    return num ** 2;
+};
+
+// 関数の戻り値を出力する
+console.log(double(10));
